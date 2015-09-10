335e36b290d4b7d662d05a09a8157eee28a800c2
Max Baldwin <maxrbaldwin2328@gmail.com>
2015-09-10 17:59:00 -0400

    Added cmd directory for reports command

---
+++ cmds/reports/index.js
@@ -0,0 +1,9 @@
+var store = require('./store');
+
+var reports = function(server) {
+  // if there is a server value, look up that server value in the store object.
+  // If it exists get reports from that server.
+  // If not, console log, not set.
+};
+
+module.exports = reports;

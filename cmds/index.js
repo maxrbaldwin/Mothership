8ab6aba0c00184113435b5c92df0e4dfe2bc99f8
Max Baldwin <maxrbaldwin2328@gmail.com>
2015-09-15 16:42:20 -0400

    Added commands for open and help

--- cmds/index.js
+++ cmds/index.js
@@ -1,12 +1,15 @@
 var launch = require('./launch');
 var secret = require('./secret');
+var open = require('./open');
+var profile = require('./profile');
+var stop = require('./stop');
+var help = require('./help');
 
 var commands = {
   launch: launch,
   secret: secret,
-  help: function() {
-    console.log('help!');
-  }
+  open: open,
+  help: help
 };
 
 module.exports = commands;

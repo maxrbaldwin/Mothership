7dab2b7b8d2a00d94625df3231c83975d0ec3718
Max Baldwin <maxrbaldwin2328@gmail.com>
2015-09-25 18:09:36 -0400

    Promisified db functions

--- .mothership/dbs/profile/index.js
+++ .mothership/dbs/profile/index.js
@@ -1,9 +1,10 @@
 var Datastore = require('nedb');
 var path = require('path');
+var Promise = require('bluebird');
 
 var db = new Datastore({
   filename: path.join(__dirname, '/profile.db'),
   autoload: true
 });
 
-module.exports = db;
+module.exports = Promise.promisifyAll(db);
